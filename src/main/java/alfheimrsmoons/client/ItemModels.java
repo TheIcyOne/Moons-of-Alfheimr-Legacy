471329f37df7332062357d35d0de4ebadfdabe3b
MrIbby <siribby@outlook.com>
2016-05-07 18:53:42 -0700

    Fix rune log state error

--- src/main/java/alfheimrsmoons/client/ItemModels.java
+++ src/main/java/alfheimrsmoons/client/ItemModels.java
@@ -7,7 +7,9 @@ import alfheimrsmoons.init.AMBlocks;
 import alfheimrsmoons.init.AMItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
 
@@ -15,10 +17,10 @@ public class ItemModels {
     public static void registerModels() {
         registerBlock(AMBlocks.soil);
         registerBlock(AMBlocks.shale);
-        registerBlockWithVariants(AMBlocks.log, AMBlocks.log.variant, "_log");
-        registerBlockWithVariants(AMBlocks.log2, AMBlocks.log2.variant, "_log");
-        registerBlockWithVariants(AMBlocks.planks, BlockAMPlanks.VARIANT, "_planks");
-        registerBlockWithVariants(AMBlocks.ore, BlockAMOre.VARIANT, "_ore");
+        registerBlockWithVariants(AMBlocks.log, AMBlocks.log.variant, "log");
+        registerBlockWithVariants(AMBlocks.log2, AMBlocks.log2.variant, "log");
+        registerBlockWithVariants(AMBlocks.planks, BlockAMPlanks.VARIANT, "planks");
+        registerBlockWithVariants(AMBlocks.ore, BlockAMOre.VARIANT, "ore");
         registerBlock(AMBlocks.sediment);
         registerItem(AMItems.branch_bow);
         registerItem(AMItems.rock_arrow);
@@ -48,11 +50,21 @@ public class ItemModels {
         registerItem(item, item.getRegistryName().getResourcePath());
     }
 
-    private static <T extends Comparable<T>> void registerBlockWithVariants(Block block, IProperty<T> property, String suffix) {
+    private static <T extends Comparable<T>> void registerBlockWithVariants(Block block, IProperty<T> property, final String baseName) {
         for (T value : property.getAllowedValues()) {
             int meta = block.getMetaFromState(block.getDefaultState().withProperty(property, value));
-            String id = property.getName(value) + suffix;
+            String id = property.getName(value) + "_" + baseName;
             registerBlock(block, meta, id);
+        }
+
+        String blockName = block.getRegistryName().getResourcePath();
+        if (!blockName.equals(baseName)) {
+            ModelLoader.setCustomStateMapper(block, new StateMapperBase() {
+                @Override
+                protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+                    return new ModelResourceLocation(AlfheimrsMoons.MOD_ID + ":" + baseName, getPropertyString(state.getProperties()));
+                }
+            });
         }
     }
 }
