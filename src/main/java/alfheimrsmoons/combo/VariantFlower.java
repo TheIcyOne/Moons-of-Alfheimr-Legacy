7738577926079de67dd526d1679c06c212f33d8c
MrIbby <siribby@outlook.com>
2016-09-03 19:30:45 -0700

    Update flower map colors

--- src/main/java/alfheimrsmoons/combo/VariantFlower.java
+++ src/main/java/alfheimrsmoons/combo/VariantFlower.java
@@ -5,24 +5,23 @@ import zaggy1024.combo.variant.IMetadata;
 
 public enum VariantFlower implements IMetadata<VariantFlower>
 {
-    // TODO map colors
-    SNAPDRAGON("snapdragon", MapColor.FOLIAGE, true, false),
-    CARNATION("carnation", MapColor.FOLIAGE, true, false),
-    BURSTBLOOM("burstbloom", MapColor.FOLIAGE, true, false),
-    WARM_VIOLET("warm_violet", MapColor.FOLIAGE, true, false),
-    STARSINGER("starsinger", MapColor.FOLIAGE, true, false),
-    PHILODENDRON("philodendron", MapColor.FOLIAGE, true, false),
-    KNOTLEAF("knotleaf", MapColor.FOLIAGE, true, false),
-    COLOMBINE("colombine", MapColor.FOLIAGE, false, true),
-    FURCREA("furcrea", MapColor.FOLIAGE, true, true),
-    BLUEBELL("bluebell", MapColor.FOLIAGE, true, true),
-    DINANTHUS("dinanthus", MapColor.FOLIAGE, false, true),
-    PURPLE_PACIFIST("purple_pacifist", MapColor.FOLIAGE, true, false),
-    SKULLCAP("skullcap", MapColor.FOLIAGE, true, false),
-    PINK_PRESTIGE("pink_prestige", MapColor.FOLIAGE, true, false),
-    ASTER("aster", MapColor.FOLIAGE, true, false),
-    PROMISES_BURDEN("promises_burden", MapColor.FOLIAGE, true, false),
-    FORGET_ME_NOT("forget_me_not", MapColor.FOLIAGE, true, false);
+    COLOMBINE("colombine", MapColor.RED, false, true),
+    SNAPDRAGON("snapdragon", MapColor.RED, true, false),
+    CARNATION("carnation", MapColor.RED, true, false),
+    BURSTBLOOM("burstbloom", MapColor.ADOBE, true, false),
+    WARM_VIOLET("warm_violet", MapColor.YELLOW, true, false),
+    STARSINGER("starsinger", MapColor.YELLOW, true, false),
+    PHILODENDRON("philodendron", MapColor.GREEN, true, false),
+    KNOTLEAF("knotleaf", MapColor.GREEN, true, false),
+    FURCREA("furcrea", MapColor.GREEN, true, true),
+    BLUEBELL("bluebell", MapColor.BLUE, true, true),
+    PURPLE_PACIFIST("purple_pacifist", MapColor.PURPLE, true, false),
+    SKULLCAP("skullcap", MapColor.PURPLE, true, false),
+    DINANTHUS("dinanthus", MapColor.MAGENTA, false, true),
+    PINK_PRESTIGE("pink_prestige", MapColor.PINK, true, false),
+    ASTER("aster", MapColor.PINK, true, false),
+    FORGET_ME_NOT("forget_me_not", MapColor.PINK, true, false),
+    PROMISES_BURDEN("promises_burden", MapColor.SNOW, true, false);
 
     private final String name;
     private final MapColor mapColor;
